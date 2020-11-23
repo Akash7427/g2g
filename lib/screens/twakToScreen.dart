ebc76fbc1bd2d7e8b5d8c42dd8fc3ec68d6dd17b
HASAN ABBAS SORATHIYA <hasan@beedevsolutions.com>
2020-11-23 15:56:26 +0530

    Tawk Integration

--- lib/screens/twakToScreen.dart
+++ lib/screens/twakToScreen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_tawk/flutter_tawk.dart';
 import 'package:g2g/constants.dart';
 import 'package:g2g/responsive_ui.dart';
 import 'package:g2g/screens/loginScreen.dart';
+import 'package:g2g/utility/pref_helper.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:g2g/components/navigationDrawer.dart';
+import 'package:g2g/controllers/clientController.dart';
+
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class TawkToScreen extends StatefulWidget {
@@ -22,75 +26,105 @@ class _TawkToScreenState extends State<TawkToScreen> {
   double _width;
   double _pixelRatio;
   bool _isLarge;
-  InAppWebViewController webView;
-  String url = "";
-  double progress = 0;
+  String name;
+  String email;
 
   @override
   void initState() {
+    getdata();
     super.initState();
+  }
+
+  getdata() async {
+    await ClientController().getClientBasic();
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      name = prefs.getString(PrefHelper.PREF_FULLNAME) ?? '';
+      email = prefs.getString(PrefHelper.PREF_EMAIL_ID) ?? '';
+      print(name);
+      print(email);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    _height = MediaQuery
-        .of(context)
-        .size
-        .height;
-    _pixelRatio = MediaQuery
-        .of(context)
-        .devicePixelRatio;
-    _width = MediaQuery
-        .of(context)
-        .size
-        .width;
+    _height = MediaQuery.of(context).size.height;
+    _pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    _width = MediaQuery.of(context).size.width;
     _isLarge = ResponsiveWidget.isScreenLarge(_width, _pixelRatio);
-
     return Scaffold(
-        body: Container(
-            child: Column(children: <Widget>[
-              Expanded(
-                child: Container(
-                  child: InAppWebView(
-                    initialUrl: "https://tawk.to/chat/580d5a22d0f23f0cd8dc1448/default",
-                    initialHeaders: {},
-                    initialOptions: InAppWebViewGroupOptions(
-                        crossPlatform: InAppWebViewOptions(
-                          debuggingEnabled: true,
-                        )
-                    ),
-                    onWebViewCreated: (InAppWebViewController controller) {
-                      webView = controller;
-                    },
-                    onLoadStart: (InAppWebViewController controller,
-                        String url) async{
-                      String result2 = await controller.evaluateJavascript(
-                          source: """
-                       <script type="text/javascript">
-var Tawk_API=Tawk_API||{};
-Tawk_API.visitor = {
-name : 'visitor name',
-email : 'visitor@email.com'
-};
-
-var Tawk_LoadStart=new Date();
-<!-- rest of the tawk.to widget code 
-</script>
-                      """);
-                      print(result2);
-
-                    },
-                    onLoadStop: (InAppWebViewController controller,
-                        String url) async {
-
-                     // Foo Bar
-
-                    },
-                  ),
+      key: _connectScreenKey,
+      drawer: NavigationDrawer(),
+      body: Stack(children: [
+        new Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: const AssetImage('images/bg.jpg'), fit: BoxFit.cover)),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 10.0, left: 10.0),
+          child: AppBar(
+            leading: CircleAvatar(
+              radius: 25,
+              backgroundColor: Color(0xffccebf2),
+              child: IconButton(
+                onPressed: () {
+                  print('abc');
+                  _connectScreenKey.currentState.openDrawer();
+                },
+                icon: Icon(
+                  Icons.menu,
+                  color: kSecondaryColor,
+                  size: _isLarge ? 35 : 30,
                 ),
               ),
-            ]))
+            ),
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(),
+                CircleAvatar(
+                  radius: 25,
+                  backgroundColor: Color(0xffccebf2),
+                  child: IconButton(
+                    onPressed: () {
+                      launch("tel://1300197727");
+                    },
+                    icon: Icon(
+                      Icons.call,
+                      color: kSecondaryColor,
+                      size: _isLarge ? 35 : 30,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+          ),
+        ),
+        Positioned(
+          top: 110.0,
+          left: 0.0,
+          bottom: 0.0,
+          right: 0.0,
+          child: Container(
+            padding: EdgeInsets.all(10),
+            child: Tawk(
+              directChatLink:
+                  'https://tawk.to/chat/580d5a22d0f23f0cd8dc1448/default',
+              visitor: TawkVisitor(
+                name: name,
+                email: email,
+              ),
+            ),
+          ),
+        ),
+      ]),
     );
+
     // return WebviewScaffold(
     //   key: _connectScreenKey,
     //   appBar: AppBar(
@@ -136,9 +170,40 @@ var Tawk_LoadStart=new Date();
     //   withJavascript: true,
     //   url: 'https://tawk.to/chat/5f3278b420942006f46a9dc2/default',
     //   initialChild:
-    //   SpinKitPouringHourglass(color: kPrimaryColor, size: _width * 0.25),
+    //       SpinKitPouringHourglass(color: kPrimaryColor, size: _width * 0.25),
     //   hidden: true,
     // );
+    // Scaffold(
+    //   key: _connectScreenKey,
+    //   drawer: NavigationDrawer(),
+    //   appBar:
 
+    //   body:
+    //   SafeArea(
+    //           child:  WebView(
+    //      initialUrl: 'https://tawk.to/chat/5f3278b420942006f46a9dc2/default',
+    //      javascriptMode: JavascriptMode.unrestricted,
+
+    //      onWebViewCreated: (WebViewController webViewController) async {
+    //             _controller=webViewController;
+    //          webViewController.loadUrl('https://tawk.to/chat/5f3278b420942006f46a9dc2/default');
+
+    //         },
+    //     ),
+    //   )
+    // );
   }
 }
+// <!--Start of Tawk.to Script-->
+// <script type="text/javascript">
+// "var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// (function(){
+// var s1=document.createElement(\"script\"),s0=document.getElementsByTagName(\"script\")[0];
+// s1.async=true;
+// s1.src='https://embed.tawk.to/5f3278b420942006f46a9dc2/default';
+// s1.charset=\'UTF-8\';
+// s1.setAttribute(\'crossorigin\',\'*\');
+// s0.parentNode.insertBefore(s1,s0);
+// })();"
+// </script>
+// <!--End of Tawk.to Script-->
